725acadf9c50504b32d9df4e1f8634104caab114
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-10-23 00:35:09 +0700

    Update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_URL,
+        accounts: [process.env.PRIVATE_KEY]
+      }
+    }
+  }
 };
